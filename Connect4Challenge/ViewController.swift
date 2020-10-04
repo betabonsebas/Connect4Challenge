c35e1018f860ebe71f46735e4848011118381a6b
Sebastian Bonilla <betabonsebas@gmail.com>
2020-10-03 20:02:47 -0500

    Initial Commit

---
+++ Connect4Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Connect4Challenge
+//
+//  Created by Sebastian Bonilla on 3/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
